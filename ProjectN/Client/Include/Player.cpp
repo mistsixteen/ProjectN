0ccbf3d7614553c0a394a1dbd598f6b657dc1325
Unknown <mist.sixteen@gmail.com>
2017-10-07 19:11:42 +0900

    이동 구현중

--- ProjectN/Client/Include/Player.cpp
+++ ProjectN/Client/Include/Player.cpp
@@ -6,7 +6,7 @@
 HRESULT Player::Initialize(void)
 {
 	// 생선된 캐릭터 메쉬 복제
-	GET_SINGLE(MeshManager)->CloneMesh(L"Player", &information.mesh);
+	//GET_SINGLE(MeshManager)->CloneMesh(L"Player", &information.mesh);
 	
 	// 충돌 정점 설정
 	information.min = *GET_SINGLE(MeshManager)->GetMin(L"Player");
@@ -22,7 +22,20 @@ HRESULT Player::Initialize(void)
 	flying = true;
 	
 	// 낙하 속도 초기화
-	fallingSpeed = 0.f;
+	fallingSpeed = 2.0f;
+
+	// 임시 : boost 관련 변수들 초기화
+
+	boostspeed = 0.0f;
+	bospeedmax = 2.4f;
+	bospeedincrese = 0.1f;
+	bospeeddecrese = 0.3f;
+
+	burstspeed = 0.0f;
+	buspeedmax = 20.0f;
+	buspeeddecrese = 3.0f;
+
+
 
 	return S_OK;
 }
@@ -30,7 +43,7 @@ HRESULT Player::Initialize(void)
 void Player::Progress(void)
 {
 	prevPos = information.position;
-	if (GET_SINGLE(DXInput)->PushRight())
+	//if (GET_SINGLE(DXInput)->PushRight())
 		KeyCheck();
 	SetPosition();
 }
@@ -55,18 +68,27 @@ void Player::SetPosition()
 void Player::KeyCheck()
 {
 	D3DXVECTOR3 up = D3DXVECTOR3(0.f, 1.f, 0.f);
+	D3DXVECTOR3 move = D3DXVECTOR3(0.f, 0.f, 0.f);
 
 	// 직진 이동
 	if (PUSH_KEY(DIK_W))
 	{
-		D3DXVec3Normalize(&information.look, &information.look);
-		information.position += information.look * GET_SINGLE(TimeManager)->GetTime() * information.speed;
+		move = information.look;
+
+		move.y = 0.0f;
+		
+		D3DXVec3Normalize(&move, &move);
+		information.position -= move * GET_SINGLE(TimeManager)->GetTime() * information.speed;
 	}
 	// 후진 이동
 	if (PUSH_KEY(DIK_S))
 	{
-		D3DXVec3Normalize(&information.look, &information.look);
-		information.position -= information.look * GET_SINGLE(TimeManager)->GetTime() * information.speed;
+		move = information.look;
+
+		move.y = 0.0f;
+
+		D3DXVec3Normalize(&move, &move);
+		information.position += move * GET_SINGLE(TimeManager)->GetTime() * information.speed;
 	}
 	// 좌측 이동
 	if (PUSH_KEY(DIK_A))
@@ -86,6 +108,80 @@ void Player::KeyCheck()
 
 		information.position += vRight * GET_SINGLE(TimeManager)->GetTime() * information.speed;
 	}
+
+	
+
+
+	//falling
+	D3DXVECTOR3 failing;
+	failing.x = 0.0f;
+	failing.y = fallingSpeed * -1.0f;
+	failing.z = 0.0f;
+
+
+	information.position += failing * GET_SINGLE(TimeManager)->GetTime() * information.speed;
+
+	//fallingSpeed
+
+	if (PUSH_KEY(DIK_1)) //boost
+	{
+	
+		boostspeed = boostspeed + bospeedincrese;
+
+		if (boostspeed > bospeedmax)
+			boostspeed = bospeedmax;
+	}
+	else
+	{
+		boostspeed = boostspeed - bospeeddecrese;
+
+		if (boostspeed < 0.0f)
+			boostspeed = 0.0f;
+	}
+
+	D3DXVECTOR3 boost;
+	boost.x = 0.0f;
+	boost.y = boostspeed;
+	boost.z= 0.0f;
+
+	information.position += boost * GET_SINGLE(TimeManager)->GetTime() * information.speed;
+
+	/*
+
+	burstspeed = 0.0f;
+	buspeedmax = 10.0f;
+	buspeeddecrese = 3.0f;
+	*/
+
+	if (PUSH_KEY(DIK_2)) //burst
+	{
+		if (burstspeed == 0)
+		{
+			burstspeed = buspeedmax;
+		}
+		else
+		{
+			burstspeed = burstspeed - buspeeddecrese;
+		}
+	}
+	else
+	{
+		burstspeed = burstspeed - buspeeddecrese;
+	}
+
+	if (burstspeed < 0.0f)
+		burstspeed = 0.0f;
+
+
+	D3DXVECTOR3 burst;
+	burst.x = 0.0f;
+	burst.y = burstspeed;
+	burst.z = 0.0f;
+
+	information.position += burst * GET_SINGLE(TimeManager)->GetTime() * information.speed;
+
+
+
 }
 
 void Player::Render(void)
@@ -97,8 +193,8 @@ void Player::Render(void)
 	GET_SINGLE(DXFramework)->Drawtext(strTmp, 0, 0);*/
 
 	// 해당 오브젝트 회전 실험
-	GET_SINGLE(DXFramework)->GetDevice()->SetRenderState(D3DRS_FILLMODE, D3DFILL_WIREFRAME);
-	information.mesh->DrawSubset(0);
+	/*GET_SINGLE(DXFramework)->GetDevice()->SetRenderState(D3DRS_FILLMODE, D3DFILL_WIREFRAME);
+	information.mesh->DrawSubset(0);*/
 }
 
 void Player::Release(void)
--- ProjectN/Client/Include/Player.h
+++ ProjectN/Client/Include/Player.h
@@ -10,6 +10,18 @@ private:
 private:
 	bool			flying;
 	float			fallingSpeed;
+
+	float			movespeed;
+
+	float			boostspeed;
+	float			burstspeed;
+
+	float			bospeedmax;
+	float			bospeedincrese;
+	float			bospeeddecrese;
+
+	float			buspeedmax;
+	float			buspeeddecrese;
 public:
 	void SetPosition();
 	void KeyCheck();
